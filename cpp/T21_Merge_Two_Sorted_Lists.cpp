342c07d33af34349f9e7cf591f9a3acc0f7f8e89
djfengGit <djfenghust@hust.edu.cn>
2019-05-03 19:05:43 +0800

    add T21; add folders:cpp;Java;python3

---
+++ cpp/T21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,37 @@
+/*- 两种方法：1、模拟老师帮小学生排队，以l1为已排好的队列，将l2元素插进去；较麻烦，需要单独考虑是否已到链表尾的情况；
+            2、合并排序思路，引入头结点便于操作；
+            本题采用方法2
+  -*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy_head = new ListNode(0);
+        ListNode* ptr = dummy_head;
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val <= l2->val) {
+                ptr->next = l1;
+                l1 = l1->next;
+            } else {
+                ptr->next = l2;
+                l2 = l2->next;
+            }
+            ptr = ptr->next;
+        }
+        if (l1 == NULL) {
+            ptr->next = l2;
+        } else {
+            ptr->next = l1;
+        }
+        
+        return dummy_head->next;
+    }
+};
